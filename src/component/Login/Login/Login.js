d188b98197c7406a72b24805dfc6a14bcbe6d886
Lukman Hossain <lukman.webdev@gmail.com>
2022-01-03 17:51:51 +0600

    login and register form add

---
+++ src/component/Login/Login/Login.js
@@ -0,0 +1,77 @@
+import React from 'react'; // , { useState } , useLocation, useHistory , CircularProgress, Alert 
+import { Container, Grid, TextField, Typography, Button} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+// import useAuth from '../../../hooks/useAuth';
+
+
+const Login = () => {
+
+    // const [loginData, setLoginData] = useState({});
+
+    // const { user, loginUser, signInWithGoogle, isLoading, authError } = useAuth();
+
+    // const location = useLocation();
+    // const history = useHistory();
+
+
+    // const handleOnChange = e => {
+    //     const field = e.target.name;
+    //     const value = e.target.value;
+    //     const newLoginData = { ...loginData };
+    //     newLoginData[field] = value;
+    //     setLoginData(newLoginData);
+    // }
+
+    // const handleLogin = e => {
+    //     loginUser(loginData.email, loginData.password, location, history);
+    //     e.preventDefault();
+    // }
+
+    // const handleGoogleSignIn = () => {
+    //     signInWithGoogle(location, history)
+    // }
+    return (
+        <Container>
+            <Grid container spacing={2}>
+                <Grid item sx={{ mt: 10 }} xs={12} md={6}>
+                    <Typography variant="h5" gutterBottom>
+                        Login Here
+                    </Typography>
+                    {/* onSubmit={handleLogin} */}
+                    <form>
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Email"
+                            name="email"
+                            // onChange={handleOnChange}
+                            variant="standard" />
+                        <TextField
+                            sx={{ width: '75%', m: 1 }}
+                            id="standard-basic"
+                            label="Password"
+                            type="password"
+                            name="password"
+                            // onChange={handleOnChange}
+                            variant="standard" />
+                        <Button sx={{ width: '75%', mt: 2 }} type="submit" variant="contained">Login</Button>
+                        <NavLink style={{ textDecoration: 'none' }} to="/register">
+                            <Button variant="text">New User? Please Register</Button>
+                        </NavLink>
+                        {/* {isLoading && <CircularProgress />} 
+                        {user?.email && <Alert severity="success">Successfully Login Your Account</Alert>}
+                        {authError && <Alert severity="error">{authError}</Alert>} */}
+                    </form>
+                    <p>-------------- OR --------------</p>
+                    {/* onClick={handleGoogleSignIn} */}
+                    <Button  variant="contained">Google Sign In</Button>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <img style={{ width: '100%' }} src="https://i.ibb.co/7j19CbV/Login.jpg" alt="" />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
+
+export default Login;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,108 @@
+import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import initializeAuthentication from "../component/Login/Firebase/firebase.init";
+
+// import initializeAuthentication from "../Pages/Login/Firebase/firebase.init";
+
+initializeAuthentication();
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [authError, setAuthError] = useState('');
+
+    const auth = getAuth();
+    const googleProvider = new GoogleAuthProvider();
+
+    const registerUser = (name, email, password, history) => {
+        setIsLoading(true);
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            setAuthError('');
+            const newUser = { displayName: name, email };
+                setUser(newUser);
+
+                // saveUser(email, name, 'POST');
+
+
+          // Update Profile
+          updateProfile(auth.currentUser, {
+            displayName: name
+        }).then(() => {
+        }).catch((error) => {
+        });
+        history.replace('/');
+    })
+    .catch((error) => {
+        setAuthError(error.message);
+    })
+    .finally(() => setIsLoading(false));
+}
+
+
+    const loginUser = (email, password, location, history) => {
+        setIsLoading(true);
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const destination = location?.state?.from || '/';
+            history.replace(destination);
+            setAuthError('');
+          })
+          .catch((error) => {
+            setAuthError(error.message);
+          })
+          .finally(() => setIsLoading(false));
+    }
+
+    const signInWithGoogle = (location, history) => {
+        setIsLoading(true);
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const user = result.user;
+                // saveUser(user.email, user.displayName, 'PUT')
+                setAuthError('');
+                const destination = location?.state?.from || '/';
+                history.replace(destination);
+            }).catch((error) => {
+                setAuthError(error.message);
+            })
+            .finally(() => setIsLoading(false));
+
+    }
+
+    // Observe user state change
+    useEffect(() => {
+        const unSubscribed = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            } else {
+                setUser({});
+            }
+            setIsLoading(false);
+        });
+        return () => unSubscribed;
+    }, [auth])
+    
+    const logOut = () => {
+        setIsLoading(true);
+        signOut(auth).then(() => {
+        }).catch((error) => {
+        })
+            .finally(() => setIsLoading(false));
+    }
+
+    return {
+        user,
+        isLoading,
+        authError,
+        signInWithGoogle,
+        registerUser,
+        loginUser,
+        logOut,
+    }
+}
+
+export default useFirebase;
