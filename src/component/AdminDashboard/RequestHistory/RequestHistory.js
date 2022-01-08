66aab7e785b3441d67f5642077ce61c5f5b81a96
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-01-08 19:06:11 +0600

    heroku link updated

--- src/component/AdminDashboard/RequestHistory/RequestHistory.js
+++ src/component/AdminDashboard/RequestHistory/RequestHistory.js
@@ -1,21 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Table } from 'react-bootstrap';
-import './RequestHistory.css'
+import React, { useEffect, useState } from "react";
+import { Table } from "react-bootstrap";
+import "./RequestHistory.css";
 
 const RequestHistory = () => {
-  const[bloodrequest,setBloodrequest]=useState([])
-  
+  const [bloodrequest, setBloodrequest] = useState([]);
 
-  useEffect(()=>{
-    fetch('http://hidden-coast-99117.herokuapp.com/bloodRequest')
-    .then(res => res.json())
-    .then(data =>{
-    setBloodrequest(data)
-
-      
-      
-    })
-  },[])
+  useEffect(() => {
+    fetch("https://hidden-coast-99117.herokuapp.com/bloodRequest")
+      .then((res) => res.json())
+      .then((data) => {
+        setBloodrequest(data);
+      });
+  }, []);
 
   if (!bloodrequest?.length) {
     return (
@@ -30,54 +26,51 @@ const RequestHistory = () => {
     );
   }
 
-
-    return (
-        <div>
-        <div>
-        <h4 className='donor-details mt-3'>Request History</h4>
-        <div className='pt-3'>
-        <Table striped brequestblooded hover>
-          <thead>
-            <tr className="t-head">
-              <th>Sl</th>
-              <th>Name</th>
-              <th>Age</th>
-              <th>Gender</th>
-              <th>Blood Group</th>
-              <th>Address</th>
-              <th>Reason</th>
-              <th>Doctor Name</th>
-              <th>Mobile</th>
-              <th>Date</th>
-              <th>Quantity</th>
-              <th>Status</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bloodrequest?.map((requestblood, index) => (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{requestblood?.name}</td>
-                <td>{requestblood?.age}</td>
-                <td>{requestblood?.gender}</td>
-                <td>{requestblood?.bloodGroup}</td>
-                <td>{requestblood?.address}</td>
-                <td>{requestblood?.reason}</td>
-                <td>{requestblood?.doctorName}</td>
-                <td>{requestblood?.mobile}</td>
-                <td>{requestblood?.requestDate}</td>
-                <td>{requestblood?.quantity}</td>
-                <td>{requestblood?.status}</td>
-                
+  return (
+    <div>
+      <div>
+        <h4 className="donor-details mt-3">Request History</h4>
+        <div className="pt-3">
+          <Table striped brequestblooded hover>
+            <thead>
+              <tr className="t-head">
+                <th>Sl</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Blood Group</th>
+                <th>Address</th>
+                <th>Reason</th>
+                <th>Doctor Name</th>
+                <th>Mobile</th>
+                <th>Date</th>
+                <th>Quantity</th>
+                <th>Status</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {bloodrequest?.map((requestblood, index) => (
+                <tr>
+                  <td>{index + 1}</td>
+                  <td>{requestblood?.name}</td>
+                  <td>{requestblood?.age}</td>
+                  <td>{requestblood?.gender}</td>
+                  <td>{requestblood?.bloodGroup}</td>
+                  <td>{requestblood?.address}</td>
+                  <td>{requestblood?.reason}</td>
+                  <td>{requestblood?.doctorName}</td>
+                  <td>{requestblood?.mobile}</td>
+                  <td>{requestblood?.requestDate}</td>
+                  <td>{requestblood?.quantity}</td>
+                  <td>{requestblood?.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
         </div>
-
+      </div>
     </div>
-    </div>
-    );
+  );
 };
 
 export default RequestHistory;
