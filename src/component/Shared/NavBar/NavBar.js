a18a5d5392abb082910d35b47d3fa7ee745e498c
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-01-09 19:34:13 +0600

    title and favicon added

--- src/component/Shared/NavBar/NavBar.js
+++ src/component/Shared/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./NavBar.css";
 import useAuth from "../../../hooks/useAuth";
-import gif from '../../../images/blood-donate.gif';
+import gif from "../../../images/blood-donate.gif";
 import { Link } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 
@@ -16,12 +16,19 @@ const NavBar = () => {
     <nav className="navbar">
       <div className="nav-container">
         <NavLink exact to="/" className=" pb-2 nav-logo text-left d-flex">
-        <img className="me-3 rounded-circle" src={gif} alt="" width="58" height="52" />
-            <div className="nav-name">
-            Blood-Bank
-            </div>
+          <img
+            className="me-3 rounded-circle"
+            src={gif}
+            alt=""
+            width="58"
+            height="52"
+          />
+          <div className="nav-name">Life Blood-Bank</div>
         </NavLink>
-        <ul id="item-container" className={click ? "nav-menu active" : "nav-menu"}>
+        <ul
+          id="item-container"
+          className={click ? "nav-menu active" : "nav-menu"}
+        >
           <li className="nav-item">
             <NavLink
               exact
@@ -57,22 +64,21 @@ const NavBar = () => {
             </NavLink>
           </li>
 
-          {
-             admin === true ? 
+          {admin === true ? (
+            <li className="nav-item">
+              <NavLink
+                to="/admindashboard"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+                className="menu-name"
+              >
+                Dashboard
+              </NavLink>
+            </li>
+          ) : (
+            user?.email && (
               <li className="nav-item">
-                <NavLink
-                  to="/admindashboard"
-                  activeClassName="active"
-                  className="nav-links"
-                  onClick={handleClick}
-                  className="menu-name"
-                >
-                  Dashboard
-                </NavLink>
-              </li>
-              :
-               user?.email && 
-                <li className="nav-item">
                 <NavLink
                   to="/userdashboard"
                   activeClassName="active"
@@ -83,7 +89,8 @@ const NavBar = () => {
                   Dashboard
                 </NavLink>
               </li>
-          }
+            )
+          )}
 
           <li className="nav-item">
             <NavLink
@@ -97,17 +104,17 @@ const NavBar = () => {
             </NavLink>
           </li>
           <li className="nav-item ">
-          {user.email ? (
-                <Nav.Link >
-                  <button className="btn btn-danger" onClick={logOut}>
-                    LOGOUT
-                  </button>
-                </Nav.Link>
-              ) : (
-                <Nav.Link as={Link} to="/login">
-                  <button className="btn btn-danger">LOGIN</button>
-                </Nav.Link>
-              )}
+            {user.email ? (
+              <Nav.Link>
+                <button className="btn btn-danger" onClick={logOut}>
+                  LOGOUT
+                </button>
+              </Nav.Link>
+            ) : (
+              <Nav.Link as={Link} to="/login">
+                <button className="btn btn-danger">LOGIN</button>
+              </Nav.Link>
+            )}
           </li>
           <li className="user-name">{user?.displayName}</li>
         </ul>
